75e8c8f0fb2af717bb398ad95f7319a2f1c3ed73
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-11-10 16:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+
+let namePattern = /^[A-Za-z]{3,}$/
+let emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+let mobilePattern = /^[0-9]{10}$/
+
+
+let name = document.getElementById("#name")
+let nameValue = name.value
+
+let email = document.getElementById("#email")
+let emailValue = email.value
+
+let number = document.getElementById("#number")
+let numberValue = number.value
+
+function validName() {
+	if(namePattern.test(nameValue)){
+		name.style.backgroundcolor = "lightgreen"
+	}
+}
+function validEmail() {
+	if(namePattern.test(emailValue)){
+		email.style.backgroundcolor = "lightgreen"
+	}
+}
+function validNumber() {
+	if(namePattern.test(numberValue)){
+		number.style.backgroundcolor = "lightgreen"
+	}
+}
